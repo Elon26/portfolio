85d2bb531835e1f4c526319be23cd8cfe8187059
Evgeniy <evgeniy.sologub.vl@gmail.com>
2023-01-06 09:33:53 +1000

    upload

---
+++ src/components/CV.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CV = () => {
+    return (
+        <div className="cv">
+            <h2 className="cv__header header">Моё резюме:</h2>
+            <div className="cv__item">
+                <a href="https://vladivostok.hh.ru/resume/a2cc2532ff0b90145c0039ed1f4d6e6d376e62?hhtmFrom=resume_list" target="blank">Перейти на HeadHunter</a>
+            </div>
+        </div>);
+};
+
+export default CV;
---
+++ src/components/work.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Work = ({ name, img, stack, repository, deploy, video, description }) => {
+    return (
+        <div className="works__item">
+            <div className={"works__card " + (description.length > 500 ? "big" : "small")}>
+                <div className="works__front">
+                    <div className="works__screen">
+                        <img src={img} alt="Скрин приложения" />
+                    </div>
+                    <div className="works__name">{name}</div>
+                </div>
+                <div className="works__back">
+                    <div className="works__info">
+                        <div className="works__infoItem">
+                            <b>Название проекта: </b>{name}
+                        </div>
+                        <div className="works__infoItem">
+                            <b>Стек технологий: </b>{stack}
+                        </div>
+                        <div className="works__infoItem">
+                            <a href={repository}
+                                target="blank">Перейти в репозиторий GitHub</a>
+                        </div>
+                        <div className="works__infoItem">
+                            <a href={deploy}
+                                target="blank">Посмотреть deploy проекта</a>
+                        </div>
+                        {video && <div className="works__infoItem">
+                            <a href={video}
+                                target="blank">Посмотреть видеопрезентацию проекта</a>
+                        </div>}
+                        <div className="works__infoItem">
+                            <b>Описание проекта: </b>{description}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
+    );
+};
+
+export default Work;
