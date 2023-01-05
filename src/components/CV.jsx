ecba4463218574b86fdf9e24bc787d962fa0abf7
Evgeniy <evgeniy.sologub.vl@gmail.com>
2023-01-06 09:46:39 +1000

    update CV

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -5,7 +5,12 @@ const CV = () => {
         <div className="cv">
             <h2 className="cv__header header">Моё резюме:</h2>
             <div className="cv__item">
-                <a href="https://vladivostok.hh.ru/resume/a2cc2532ff0b90145c0039ed1f4d6e6d376e62?hhtmFrom=resume_list" target="blank">Перейти на HeadHunter</a>
+                <p>
+                    <a href="https://vladivostok.hh.ru/resume/a2cc2532ff0b90145c0039ed1f4d6e6d376e62?hhtmFrom=resume_list" target="blank">Перейти на HeadHunter</a>
+                </p>
+                <p>
+                    <a href="https://firebasestorage.googleapis.com/v0/b/diplom-project-es2612.appspot.com/o/portfolio%2FCV.pdf?alt=media&token=b0c59bdb-d2ea-4bdf-b3d1-14dd53c7964d" target="blank">Посмотреть сохраненную копию</a>
+                </p>
             </div>
         </div>);
 };
