5204ecae4939f8a2d73cb18bbe8f1011ada50a54
elon26 <evgeniy.sologub.vl@gmail.com>
2024-12-12 11:50:31 +0400

    upload

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,6 +9,7 @@ const About = () => {
                 <p>🔧 I enjoy this field because it feels like solving puzzles. On one hand, it involves finding technical solutions to coding challenges. On the other hand, it’s about understanding the purpose of an assigned task and working with the client to refine it, ultimately enhancing the quality of the final product.</p>
                 <p>🌿 Throughout my career, I have contributed to multiple projects using different frameworks, gaining diverse experience in Svelte, Vue, and React. I have a successful track record of delivering projects ahead of schedule.</p>
                 <p>🏋️‍♂️ I am dedicated to continuous learning and growth and consistently adapting to new challenges. My sense of responsibility, strong self-motivation, and ability to balance immediate tasks with a focus on the bigger picture make me a versatile and valuable member of any team.</p>
+                <p>🔑 My Tech Stack: HTML, SCSS, JavaScript, TypeScript, React, Redux, Next, Svelte 4, Svelte 5, SvelteKit, Vue, Quasar, Git, ClickUp.</p>
                 <p>🚀 Let's connect and explore how we can work together to create outstanding web experiences!!!</p>
             </div>
         </div>);
