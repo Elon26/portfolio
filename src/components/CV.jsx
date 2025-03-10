b7dd5e72703007774d6a29c0364ed281c2e3497f
elon26 <evgeniy.sologub.vl@gmail.com>
2025-03-10 07:08:35 +0400

    upload

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -16,10 +16,10 @@ const CV = () => {
                     {lang === 'en' && <a href="https://hh.ru/resume/a2cc2532ff0b90145c0039ed1f4d6e6d376e62/" target="blank">{langs[lang].cv.headhunter}</a>}
                     {lang === 'ru' && <a href="https://hh.ru/resume/dafca8a1ff0e0fc8640039ed1f565466446259/" target="blank">{langs[lang].cv.headhunter}</a>}
                 </p>
-                <p>
+                {/* <p>
                     {lang === 'en' && <a href="https://firebasestorage.googleapis.com/v0/b/diplom-project-es2612.appspot.com/o/portfolio%2FSologub_Evgenii_CV.pdf?alt=media&token=2b5093fe-eb09-4c24-bd7c-dc69f24b24d3" target="blank">{langs[lang].cv.download}</a>}
                     {lang === 'ru' && <a href="https://firebasestorage.googleapis.com/v0/b/diplom-project-es2612.appspot.com/o/portfolio%2F%D0%A1%D0%BE%D0%BB%D0%BE%D0%B3%D1%83%D0%B1_%D0%95%D0%B2%D0%B3%D0%B5%D0%BD%D0%B8%D0%B8%CC%86_CV.pdf?alt=media&token=f311e71d-aaf3-4e50-88fe-2205acea79b3" target="blank">{langs[lang].cv.download}</a>}
-                </p>
+                </p> */}
             </div>
         </div>);
 };
