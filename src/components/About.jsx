6b35a2a34ff40dd34da2552775d714f1e7b4e70e
elon26 <evgeniy.sologub.vl@gmail.com>
2025-04-16 12:15:31 +0400

    upload

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,15 +3,18 @@ import langs from "../lang/langs.json";
 import { useLangs } from "../hooks/useLangs";
 
 const About = () => {
-    const { lang } = useLangs();
+  const { lang } = useLangs();
 
-    return (
-        <div className="about">
-            <h2 className="about__header header">{langs[lang].about.title}</h2>
-            <div className="about__content">
-                {langs[lang].about.content.map(work => <p>{work}</p>)}
-            </div>
-        </div>);
+  return (
+    <div className="about">
+      <h2 className="about__header header">{langs[lang].about.title}</h2>
+      <div className="about__content">
+        {langs[lang].about.content.map((work) => (
+          <p>{work}</p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default About;
